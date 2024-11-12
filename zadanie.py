ea695af9706094dcef84dcfb406fec5725b1f002
djonkovalev <djon9489@gmail.com>
2024-11-12 21:07:17 +0700

    Add files via upload

---
+++ zadanie.py
@@ -0,0 +1,9 @@
+print ((9**0.5) * 5) # "1 st program"
+print (9.99 > 9.98 and 1000 != 1000.1) # "2 nd program"
+print (2 * 2 + 2) # "3 rd program"
+print (2 * (2 + 2))
+print ((2 * 2 + 2) == (2 * (2 + 2)))
+print(type(123.456))  #"4 th program"
+print (123.456 * 10)
+print (type(int(1234.56)))
+print (1234 / 10)
